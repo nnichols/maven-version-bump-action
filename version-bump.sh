b58126a31c27a67d25265f7f491fd8328132ba74
Andrew <drewkarp@gmail.com>
2023-12-07 18:01:07 +0200

    changed location of pompath for subdirectories

--- version-bump.sh
+++ version-bump.sh
@@ -68,7 +68,7 @@ else
   echo $BUMP_MODE "version bump detected"
   bump $BUMP_MODE $OLD_VERSION
   echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
-  mvn -q versions:set -DnewVersion="${NEW_VERSION}"
+  mvn --file $POMPATH/pom.xml -q versions:set -DnewVersion="${NEW_VERSION}"
   git add $POMPATH/pom.xml
   REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
   git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
